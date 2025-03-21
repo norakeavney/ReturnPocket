67f356fe73ed61f18bbb71bfb6cd9990ec41bfc4
norakeavney <g00415845@atu.ie>
2025-03-21 13:57:44 +0000

    Enhance receipt card styling and implement receipt loading from SQLite database

--- return-pocket/src/app/pages/receipts/receipts.page.ts
+++ return-pocket/src/app/pages/receipts/receipts.page.ts
@@ -1,16 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { SqliteService } from '../../services/sqlite.service';
 
 @Component({
   standalone: false,
   selector: 'app-receipts',
   templateUrl: './receipts.page.html',
   styleUrls: ['./receipts.page.scss'],
+  providers: [SqliteService]
 })
 export class ReceiptsPage implements OnInit {
 
-  constructor() { }
+  receipts: any[] = [];
 
-  ngOnInit() {
+  constructor() {}
+
+  async ngOnInit() {
   }
+  
 
+  async loadReceipts() {
+    try {
+      console.log("🔍 Loading receipts...");
+      //this.receipts = await SqliteService.getReceipts();
+      console.log("📦 Loaded Receipts:", this.receipts);
+    } catch (error) {
+      console.error("❌ Error loading receipts:", error);
+    }
+  }
 }
+
